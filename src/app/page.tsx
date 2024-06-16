4f7185fb32ad4c6baa391f613a067be3c8f79c4d
Jorge Daniel Sosa <jdsosa@gmail.com>
2024-06-16 13:50:49 +0100

    Feat: Adds background image with social network links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,91 +4,52 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
+      <div className={styles.container}>
+        <Image
+          src="/images/banner.png"
+          alt="EAMusic banner"
+          className={styles.banner}
+          priority
+          fill
+        />
+        <div className={styles.linkContainer}>
           <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            className={styles.link}
+            href="https://www.instagram.com/eanarmusic"
             target="_blank"
-            rel="noopener noreferrer"
           >
-            By{" "}
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
+              src="/images/icons/instagram.svg"
+              alt="Instagram link"
+              width={50}
+              height={50}
+            />
+          </a>
+          <a
+            className={styles.link}
+            href="https://www.youtube.com/@Eanarmusic"
+            target="_blank"
+          >
+            <Image
+              src="/images/icons/youtube.svg"
+              alt="Youtube link"
+              width={50}
+              height={50}
+            />
+          </a>
+          <a
+            className={styles.link}
+            href="https://www.tiktok.com/@eanarmusic"
+            target="_blank"
+          >
+            <Image
+              src="/images/icons/tiktok.svg"
+              alt="TikTok link"
+              width={50}
+              height={50}
             />
           </a>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
   );
