4d2398e6e82816021addc73ca442b23d80650aea
Jorge Daniel Sosa <jdsosa@gmail.com>
2024-06-16 14:04:04 +0100

    Feat: Adds owl image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,43 +12,48 @@ export default function Home() {
           priority
           fill
         />
-        <div className={styles.linkContainer}>
-          <a
-            className={styles.link}
-            href="https://www.instagram.com/eanarmusic"
-            target="_blank"
-          >
-            <Image
-              src="/images/icons/instagram.svg"
-              alt="Instagram link"
-              width={50}
-              height={50}
-            />
-          </a>
-          <a
-            className={styles.link}
-            href="https://www.youtube.com/@Eanarmusic"
-            target="_blank"
-          >
-            <Image
-              src="/images/icons/youtube.svg"
-              alt="Youtube link"
-              width={50}
-              height={50}
-            />
-          </a>
-          <a
-            className={styles.link}
-            href="https://www.tiktok.com/@eanarmusic"
-            target="_blank"
-          >
-            <Image
-              src="/images/icons/tiktok.svg"
-              alt="TikTok link"
-              width={50}
-              height={50}
-            />
-          </a>
+        <div className={styles.content}>
+          <div className={styles.owl}>
+            <Image src="/images/owl.png" alt="Owl image" priority fill />
+          </div>
+          <div className={styles.linkContainer}>
+            <a
+              className={styles.link}
+              href="https://www.instagram.com/eanarmusic"
+              target="_blank"
+            >
+              <Image
+                src="/images/icons/instagram.svg"
+                alt="Instagram link"
+                width={50}
+                height={50}
+              />
+            </a>
+            <a
+              className={styles.link}
+              href="https://www.youtube.com/@Eanarmusic"
+              target="_blank"
+            >
+              <Image
+                src="/images/icons/youtube.svg"
+                alt="Youtube link"
+                width={50}
+                height={50}
+              />
+            </a>
+            <a
+              className={styles.link}
+              href="https://www.tiktok.com/@eanarmusic"
+              target="_blank"
+            >
+              <Image
+                src="/images/icons/tiktok.svg"
+                alt="TikTok link"
+                width={50}
+                height={50}
+              />
+            </a>
+          </div>
         </div>
       </div>
     </main>
